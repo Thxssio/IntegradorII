3c22f05dfcf39c93f17ae42ea6e177fbd0a8bb49
thxssio <thxssio@gmail.com>
2024-08-21 01:44:15 -0300

    Updated Graphics

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,9 +1,28 @@
 import axios from 'axios';
 const API_URL = process.env.REACT_APP_API_URL;
+
 // Função para obter dados do InfluxDB
 export const getData = async () => {
     const response = await axios.get(`${API_URL}/influx-data`);
-    return response.data;
+    
+    const influxData = response.data;
+
+    // Verifica se influxData é um array
+    if (Array.isArray(influxData)) {
+        if (influxData.length === 0) {
+            throw new Error('Nenhum dado disponível');
+        }
+
+        // Ordena os dados pelo timestamp mais recente
+        influxData.sort((a, b) => new Date(b.time) - new Date(a.time));
+
+        // Seleciona o dado mais recente
+        const mostRecentData = influxData[0];
+
+        return mostRecentData; // Retorna apenas o dado mais recente
+    } else {
+        throw new Error('Dados retornados não estão no formato esperado');
+    }
 };
 
 export const updateStatus = async (id, status) => {
